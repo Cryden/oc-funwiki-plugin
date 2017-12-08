8f6391041de82a6436550160d052e9d1710afa9d
CRYDEsigN <Cryden_da@mail.ru>
2017-12-08 15:32:38 +0300

    add sidebar menu

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,8 @@
 <?php namespace Crydesign\FunWiki;
 
 use System\Classes\PluginBase;
+use Backend;
+use BackendMenu;
 
 class Plugin extends PluginBase
 {
@@ -11,4 +13,10 @@ class Plugin extends PluginBase
     public function registerSettings()
     {
     }
+
+    public function register()
+    {
+        BackendMenu::registerContextSidenavPartial('Crydesign.FunWiki', 'fun-wiki', '~\plugins\crydesign\funwiki\partials\_sidenav.htm');
+    }
+
 }
